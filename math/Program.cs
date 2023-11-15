79533bce1bea6a227108da50c6a292ba0f46ac23
user <anderevna@yandex.ru>
2023-11-15 21:20:14 +0300

    Добавьте файлы проекта.

---
+++ math/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ConsoleApp1;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        delegate double Function(double x);
+
+        static void Table(double x1, double dx, double x2, Function f)
+        {
+            double y;
+            string OutputFormat = "{0:F4} {1:F4}";
+            Console.WriteLine(OutputFormat, "x", "y");
+            Console.WriteLine("Hello World!"); //добавили вывод Hello World!
+
+
+            for (double x = x1; x <= x2; x += dx)
+            {
+                y = f(x);
+                Console.WriteLine(OutputFormat, x, y);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            double sin_(double x) { return Math.Sin(x); }
+            double cos_(double x) { return Math.Cos(x); }
+
+            Console.Clear();
+
+            Function function = sin_;
+            Table(0, Math.PI / 10, Math.PI, function);
+
+            function = cos_;
+            Table(0, Math.PI / 10, Math.PI, function);
+
+            int totalLines = System.IO.File.ReadAllLines(System.Reflection.Assembly.GetExecutingAssembly().Location).Length;
+            int totalCommentLines = 57; //изменили количество закомментированных строк
+            double commentRatio = (double)totalCommentLines / totalLines;
+            Console.WriteLine($"Уровень комментированности программы {totalCommentLines}:{totalLines}={commentRatio:F4}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
